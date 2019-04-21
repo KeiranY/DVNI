7cd841aa41c05742f7a8400c3ec1e472a300662d
Keiran Young <kcyoung1997@hotmail.co.uk>
2019-04-21 23:04:48 +0100

    Base Docker Class

---
+++ container/__init__.py
@@ -0,0 +1,73 @@
+import os
+import docker
+from mininet.log import info, debug
+from mininet import node
+
+
+class Docker(node.Docker):
+    """
+        Wrapper to mininet.node.Docker that automates building containers
+    """
+    client = docker.from_env()
+    built = []
+    added = []
+
+    def __init__(self, name, dimage, **kwargs):
+        # Prepend image names with our repo name
+        dimage = 'dvni/' + dimage
+        # Build the needed image if it doesn't exist
+        if dimage not in Docker.built:
+            self.build(dimage)
+        super(Docker, self).__init__(name, dimage, **kwargs)
+        Docker.added.append(self)
+
+    def document(self, doc):
+        # Derived classes should implement this to write documentation about themselves
+        # By default prints interfaces if they exist
+        if self.intfs:
+            p = doc.add_paragraph()
+            for key, intf in self.intfs.items():
+                p.add_run('Interface: \t%s\n' % str(intf.name)).bold = True
+                p.add_run('IP Address: \t%s/%s\n' % (str(intf.ip), str(intf.prefixLen)))
+                p.add_run('MAC: \t\t%s\n\n' % str(intf.mac))
+        pass
+
+    def build(self, name):
+        """
+            Build dockerfile from the container/ directory
+
+            Example:
+                >>> self.build("kali")
+                (<Image: 'dvni/kali:latest'>, <itertools.tee object at 0x7f8c1d698098>)
+
+            Args:
+                name (str): The directory within container/ containing the Dockerfilr
+
+            Returns:
+                (:py:class:`Image`): The built image.
+
+        """
+        # TODO: Change this to allow a list of base images based on a class variable
+        if name is not 'dvni/base':
+            # if base image isn't built
+            if not Docker.client.images.list(name="dvni/base"):
+                # Build base image
+                self.build("dvni/base")
+
+        info('*** Building ' + name + ' docker container\n')
+        # If path to Dockerfile doesn't exist
+        path = os.path.dirname(os.path.realpath(__file__)) + "/" + name[5:]
+        if not os.path.isdir(path):
+            # Raise exception with path for clarity
+            raise Exception("Container does not exist: path = %s" % path)
+
+        image = Docker.client.images.build(path=path,
+                                           rm=True)
+        if len(image.tags) == 0:
+            image.tag(name)
+        # If the log level is debug, print the build log
+        #for line in logs:
+        #    debug(line)
+        # Add this image to the built list
+        Docker.built.append(name)
+        return name
