e62fdd2e2b81b4d455730fba019de05f3fa91193
Keiran Young <kcyoung1997@hotmail.co.uk>
2019-04-23 03:14:37 +0100

    readthedocs builder file

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 if __name__ == "__main__":
-    setup(name='DVNG',
+    setup(name='DVNI',
           version='1.0',
-          description='Damn Vulnerable Network Generator',
+          description='Damn Vulnerable Network Infrastructure',
           author='Keiran Young',
-          author_email='kcyoung1997@hotmail.co.uk', requires=['docx', 'sphinx', 'docker', 'mininet', 'networkx']
+          author_email='kcyoung1997@hotmail.co.uk',
+          install_requires=['docx', 'sphinx', 'docker', 'mininet', 'networkx']
           )
