bdb294afb6895dee855d84923a5d9503633cc905
Keiran Young <kcyoung1997@hotmail.co.uk>
2019-04-23 03:19:48 +0100

    readthedocs builder file

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
           description='Damn Vulnerable Network Infrastructure',
           author='Keiran Young',
           author_email='kcyoung1997@hotmail.co.uk',
-          install_requires=['docx', 'sphinx', 'docker', 'mininet', 'networkx']
+          packages=['docx', 'sphinx', 'docker', 'mininet', 'networkx']
           )
